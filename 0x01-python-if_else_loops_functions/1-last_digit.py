14054672db07c1ea72f2cd765a982c43b7ab781f
osademe <hrh.osademe@gmail.com>
2022-06-06 11:55:57 +0100

    check type error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if str(number):
+if type(number) is int:
     last_digit = str(number)[len(str(number)) - 1]
     last = int(last_digit)
     if number < 0:
@@ -12,3 +12,5 @@ if str(number):
         print(f"Last digit of {number} is {last} and is less than 6 and not 0")
     elif last == 0:
         print(f"Last digit of {number} is {last} and is 0")
+else:
+  raise TypeError("Only integers are allowed")
