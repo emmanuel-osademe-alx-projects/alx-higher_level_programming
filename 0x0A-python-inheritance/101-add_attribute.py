f046dc3c029caf1323e8b80d98a66950cd97d6c5
osademe <hrh.osademe@gmail.com>
2022-07-08 10:50:30 +0100

    ok

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -7,8 +7,8 @@ if possible
 def add_attribute(obj, attribute, value):
     """adds a new attribute"""
     if '__dict__' not in dir(obj):
-        raise TypeError("can not add new attribute")
+        raise TypeError("can't add new attribute")
     if '__slots__' in dir(obj):
-        raise TypeError("can not add new attribute")
+        raise TypeError("can't add new attribute")
     else:
         setattr(obj, attribute, value)
