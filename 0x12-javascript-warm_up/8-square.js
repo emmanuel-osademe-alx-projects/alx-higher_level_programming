3d9b862b2d47e09245197e24ce0da3691636b226
emmanuel <mcdecoil@gmail.com>
2022-09-11 06:20:31 +0000

    task done

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = process.argv[2];
+if (parseInt(size)) {
+  for (let i = 0; i < size; i++) {
+	let square = '';
+    for (let j = 0; j < size; j++) {
+      square += 'X';
+    }
+	console.log(square);
+  }
+} else {
+  console.log('Missing size');
+}
