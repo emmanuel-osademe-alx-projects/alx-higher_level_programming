6caa2424440d9ebee8acec1ca101903f0c45dd78
emmanuel <mcdecoil@gmail.com>
2022-09-11 06:21:51 +0000

    semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,11 +2,11 @@
 const size = process.argv[2];
 if (parseInt(size)) {
   for (let i = 0; i < size; i++) {
-	let square = '';
+    let square = '';
     for (let j = 0; j < size; j++) {
       square += 'X';
     }
-	console.log(square);
+    console.log(square);
   }
 } else {
   console.log('Missing size');
