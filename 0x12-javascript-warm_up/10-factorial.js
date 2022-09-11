de719380d027f7e62ab755e319ecd5185e1ab2de
emmanuel <mcdecoil@gmail.com>
2022-09-11 06:46:33 +0000

    ok

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const arg = process.argv;
 function factorial (num) {
   if (isNaN(num) || num <= 0) {
     return 1;
   }
   return num * factorial(num - 1);
 }
-console.log(factorial(arg[2]));
+console.log(factorial(process.argv[2]));
